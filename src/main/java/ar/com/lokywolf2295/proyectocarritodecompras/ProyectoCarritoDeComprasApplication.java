1c4b1584f40c2cf53a917951e4b1406e37568b41
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-09 23:57:00 -0300

    Feat: create repository with your initial files

---
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/ProyectoCarritoDeComprasApplication.java
@@ -0,0 +1,13 @@
+package ar.com.lokywolf2295.proyectocarritodecompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCarritoDeComprasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoCarritoDeComprasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/lokywolf2295/proyectocarritodecompras/ProyectoCarritoDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.lokywolf2295.proyectocarritodecompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCarritoDeComprasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
