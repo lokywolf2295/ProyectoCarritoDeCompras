69ab5b0d750867424459d5ec97b841ba2dbd16e1
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-25 22:09:01 -0300

    Update: delete unused imports

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/service/IShoppingCartService.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/service/IShoppingCartService.java
@@ -2,8 +2,6 @@ package ar.com.lokywolf2295.proyectocarritodecompras.service;
 
 import ar.com.lokywolf2295.proyectocarritodecompras.model.dto.request.ShopingRequestDTO;
 import ar.com.lokywolf2295.proyectocarritodecompras.model.dto.response.ShopingResponseDTO;
-import ar.com.lokywolf2295.proyectocarritodecompras.model.entity.ShoppingCart;
-import org.springframework.data.domain.Page;
 
 public interface IShoppingCartService {
 
