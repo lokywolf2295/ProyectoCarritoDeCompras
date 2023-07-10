2948746e210653a41c737a241ed08d45da0081ac
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-09 22:30:58 -0300

    Update: modify ShopingRequestDTO class

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ShopingRequestDTO.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ShopingRequestDTO.java
@@ -1,20 +1,17 @@
 package ar.com.lokywolf2295.proyectocarritodecompras.model.dto.request;
 
-import ar.com.lokywolf2295.proyectocarritodecompras.model.entity.Product;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class ShopingRequestDTO {
 
     @NotNull
-    private List<Product> productsList;
-    private BigDecimal totalAmount;
     private int quantity;
     private boolean enabled;
+    private BigDecimal price;
 }
