bf5269f1f77457597db73d160dc9fac271f368ef
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-09 22:48:10 -0300

    Update: modify ShopingRequestDTO class

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ShopingRequestDTO.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ShopingRequestDTO.java
@@ -5,13 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class ShopingRequestDTO {
 
     @NotNull
+    private Long id;
+    private List<ShopingRequestDTO> cart;
     private int quantity;
     private boolean enabled;
     private BigDecimal price;
+    private BigDecimal totalAmount;
 }
