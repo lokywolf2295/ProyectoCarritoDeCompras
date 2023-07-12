e52b59539f8625b208dfa595787af93c2cdd48b9
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-12 00:05:07 -0300

    Update: modify createShoppingCart method and add appendOrder method in the IShoppingCartService interface

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/service/IShoppingCartService.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/service/IShoppingCartService.java
@@ -1,9 +1,15 @@
 package ar.com.lokywolf2295.proyectocarritodecompras.service;
 
-import ar.com.lokywolf2295.proyectocarritodecompras.model.dto.request.ShopingRequestDTO;
+import ar.com.lokywolf2295.proyectocarritodecompras.model.entity.Product;
+import ar.com.lokywolf2295.proyectocarritodecompras.model.entity.ShopOrder;
+import ar.com.lokywolf2295.proyectocarritodecompras.model.entity.ShoppingCart;
+
+import java.math.BigDecimal;
 
 public interface IShoppingCartService {
 
     //declaro los metodos a implementar en la clase CarritoServiceImpl
-     void createShoppingCart(ShopingRequestDTO shopingRequestDTO);
+     ShoppingCart createShoppingCart(Product product, int quantity, BigDecimal price);
+
+     void appendOrder(ShoppingCart cart,ShopOrder order);
 }
