d3f219c44434299751fa5854cc81664aaf5b0edb
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-11 23:44:47 -0300

    Update: add Builder annotation and price attribute in ShoppingCart class

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/entity/ShoppingCart.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/entity/ShoppingCart.java
@@ -3,6 +3,7 @@ package ar.com.lokywolf2295.proyectocarritodecompras.model.entity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import jakarta.persistence.*;
@@ -10,7 +11,10 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import java.math.BigDecimal;
+
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -32,11 +36,12 @@ public class ShoppingCart {
     private Product product; // "carritoProductos" es el atributo de la clase Producto
 
     @Column(name = "product_qty")
+    @ApiModelProperty("Cantidad de productos")
     private Integer productQuantity;
 
-    @Column(name = "enable")
-    @ApiModelProperty("Permite corroborar si el carrito esta habilitado o no")
-    private boolean enabled = true;
+    @Column(name = "price")
+    @ApiModelProperty("Precio en pesos argentinos, con 2 decimales, utilizando el . punto como separador")
+    private BigDecimal price;
 
     @ManyToOne
     @JoinColumn(name = "shop_order_id")
