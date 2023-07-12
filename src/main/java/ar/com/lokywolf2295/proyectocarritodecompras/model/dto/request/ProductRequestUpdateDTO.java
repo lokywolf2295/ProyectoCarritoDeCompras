1ce99fc47d694c2571f0d7169e297b039d39f92a
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-11 23:47:06 -0300

    Update: add NoArgsConstructor annotation in ProductRequestUpdateDTO class

--- src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ProductRequestUpdateDTO.java
+++ src/main/java/ar/com/lokywolf2295/proyectocarritodecompras/model/dto/request/ProductRequestUpdateDTO.java
@@ -2,8 +2,10 @@ package ar.com.lokywolf2295.proyectocarritodecompras.model.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProductRequestUpdateDTO {
 
